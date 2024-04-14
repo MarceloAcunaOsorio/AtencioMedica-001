7913b0263418706b2ae8e924316dc8cb4b854e31
marceloacunaosorio <m.acunao@duocuc.cl>
2024-04-14 18:48:55 -0400

    se sube proyecto

---
+++ consultamedica/src/test/java/com/atencionmedica/consultamedica/ConsultamedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atencionmedica.consultamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultamedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
