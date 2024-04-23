c21e3711c5b1b18bd295532a57318673e9dd6028
marceloacunaosorio <m.acunao@duocuc.cl>
2024-04-22 22:26:38 -0400

    se actualiza

--- consultamedica/src/main/java/com/atencionmedica/consultamedica/Model/AtencionMedica.java
+++ consultamedica/src/main/java/com/atencionmedica/consultamedica/Model/AtencionMedica.java
@@ -5,8 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,9 +27,9 @@ public class AtencionMedica
     @Column(name = "Observaciones")
     private String observaciones;
 
-    @ManyToOne
-    @JoinColumn(name = "idPaciente", nullable = false)
-    private Paciente paciente;    
+    //@ManyToOne
+    //@JoinColumn(name = "idPaciente", nullable = false)
+    //private Paciente paciente;    
 
 
   //getter
